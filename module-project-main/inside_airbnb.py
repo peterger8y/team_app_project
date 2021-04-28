0f50685a9c3edcf51f9a7718050e9b255c5fed02
jrslagle <jrslagle@gmail.com>
2021-04-28 01:42:17 -0600

    get_data() is finished except for geojson

--- module-project-main/inside_airbnb.py
+++ module-project-main/inside_airbnb.py
@@ -1,22 +1,52 @@
 import os
+import json
 import pandas as pd
 
 
 class InsideAirBnB:
+    """
+    There are 7 data files available for every location and date
+    These numbers are for New York City, 2021-02-04
+                                                Size   ,   Shape            Key
+    'data/listings.csv.gz',                     21.2 MB,   (18291, 74)      listings-dat
+    'data/calendar.csv.gz',                     34.8 MB,   (13464021, 7)    calendar-dat
+    'data/reviews.csv.gz',                      99.3 MB,   (847727, 6)      reviews-dat
+    'visualisations/listings.csv',              5.12 MB,   (37012, 16)      listings-vis
+    'visualisations/reviews.csv',               15.7 MB,   (847727, 2)      reviews-vis
+    'visualisations/neighbourhoods.csv',       0.005 MB,   (230, 2)         neighborhoods-vis
+    'visualisations/neighbourhoods.geojson',   0.604 MB,   (233, 3)         neighborhoods-geo
+    """
     def __init__(self):
-        # filepath to the index of data on insideairbnb.com
-        self.index_path = os.path.join('cache', 'inside-airbnb-index.bz2')
-        self.index = self.load_index()
+        self.locations = self.load_locations()
 
-    def load_index(self):
+    def load_locations(self):
+        cache_folder, index_filename = 'cache', 'inside-airbnb-locations.bz2'
+        locations_path = os.path.join(cache_folder, index_filename)
+
         try:
-            index = pd.read_csv(self.index_path)  # attempt to load from file
+            locations = pd.read_csv(locations_path)  # attempt to load from file
         except FileNotFoundError:
-            print("Couldn't find the cached index for insideairbnb.com, regenerating it...")
-            index = self.regenerate_index()
-        return index
+            print("Couldn't find the cached locations for insideairbnb.com, regenerating it...")
 
-    def regenerate_index(self):
+            # generate the cache folder if it's missing
+            if not os.path.exists(cache_folder):
+                os.mkdir(cache_folder)
+
+            locations = self.regenerate_locations(save_path=locations_path)
+        return locations
+
+    def regenerate_locations(self, save_path=None):
+        """
+        Compiles a new index of locations and dates from insideairbnb.com
+
+        :return: A pandas DataFrame of ['name', 'url', 'dates']
+
+        TODO: This is slow because get_dates() searches all 148,121 lines of html * 109 locations.
+        It would be more efficient to take note of line numbers between each location
+        in the first pass. Then for urls and dates, pass in starting and ending line numbers
+        to regextract() to only search the sections of the html for that location.
+        Currently, this function takes ~17 seconds to finish.
+        """
         import re
         import requests
         from collections import Counter
@@ -24,32 +54,107 @@ class InsideAirBnB:
         # 148,121 of html as a list of strings
         html = requests.get('http://insideairbnb.com/get-the-data.html').text.splitlines()
 
-        def unique(repeats):
-            # Returns only unique items. Preserves the original order.
-            return list(Counter(repeats))
-
         def regextract(pattern):
+            # Searches 'html' and returns whatever is in the first () of pattern as a unique list of strings
             matches = [re.search(pattern, line) for line in html]  # non-matches are included as None
             matches = [match.group(1) for match in matches if match]  # the if filters out the None's
-            return unique(matches)  # retains only unique matches. Preserves the original order
+            return list(Counter(matches))  # Returns only unique matches. Preserves the original order
 
-        def get_dates(place_url):
-            place_url = f'http://data.insideairbnb.com/{place_url}/([\d\-]+)/'
-            dates = regextract(pattern=place_url)
-            return unique(dates)
+        def get_dates(location_url):
+            """
+            Finds all dates that insideairbnb.com has data for, given a location
+
+            :param location_url: The place to get dates for in url form
+            :return: A list of date strings in 'YYYY-MM-DD' form
+            """
+            location_url = f'http://data.insideairbnb.com/{location_url}/([\d\-]+)/'
+            dates = regextract(pattern=location_url)
+            return json.dumps(dates)
 
         # Construct the index dataframe
-        index = pd.DataFrame({
-            'place_name': regextract(pattern='<h2>(.+)</h2>'),
-            'place_url': regextract(pattern='insideairbnb.com/(.+)/[\d\-]+/data/listings')})
-        index['dates'] = index['place_url'].apply(get_dates)
+        locations = pd.DataFrame({
+            'name': regextract(pattern='<h2>(.+)</h2>'),
+            'url': regextract(pattern='insideairbnb.com/(.+)/[\d\-]+/data/listings')})
+        locations['dates'] = locations['url'].apply(get_dates)
 
         # save index to a compressed form (to 14% the original size)
         # BZ2 compression performed better than ZIP, GZ and XZ in my trial
-        index.to_csv(self.index_path)
-        return index
+        locations.to_csv(save_path, index=False)
+        return locations
+
+    def get_location_names(self):
+        return self.locations['name'].to_list()
+
+    def get_location_urls(self):
+        return self.locations['url'].to_list()
+
+    def lookup(self, column, for_):
+        # Given part of a place name, return the value in another column for that place
+        # lookup(column='dates', for_='Francisco')
+        hits = self.locations.name.str.contains(for_)
+        if hits.sum() > 0:
+            value = self.locations.at[hits.idxmax(), column]
+            if column == 'dates':
+                value = json.loads(value)
+            return value
+        else:
+            print(f"Could not find {for_} in locations['name']")
+            return None
+
+    def get_data(self, location_name=None, date=None, data_files=None):
+        # import geopandas as gpd
+
+        if location_name:
+            location_url = self.lookup(column='url', for_=location_name)
+        else:
+            location_name = 'New York City'
+            location_url = 'united-states/ny/new-york-city'
+
+        if date is None:  # if no date is provided, assume the most recent date (first in the list)
+            date = self.lookup(column='dates', for_=location_name)[1]
+
+        if data_files is None:
+            data_files = [self.data_files[0]]
+
+        domain = 'http://data.insideairbnb.com'
+        place_date = '/'.join([domain, location_url, date])
+        urls = ['/'.join([place_date, file]) for file in data_files]
+
+        def load_df(url):
+            filetype = url.split('.')[-1]
+
+            if filetype in ['csv', 'gz']:
+                df = pd.read_csv(url)  # compression format is inferred from the filename
+            # elif filetype == 'geojson':
+            #     df = gpd.read_file(url)
+            else:
+                print(f"Can't load {url} as a DataFrame")
+                df = None
+            return df
+
+        def df_name(url):
+            filetype = url.split('.')[-1]
+            table, mode = url.split('/')[-1:-3:-1]
+            table = table.split('.')[0]
+            mode = mode[:3]
+
+            if filetype == 'geojson':
+                return f'{table}-{filetype[:3]}'
+            else:
+                return f'{table}-{mode}'
+
+        dataframes = {df_name(url): load_df(url) for url in urls}
+        return dataframes
 
 
 if __name__ == "__main__":
-    inside = InsideAirBnB()
-    print(inside.index.head())
+    airbnb = InsideAirBnB()
+    # airbnb.regenerate_locations()
+    # print(airbnb.locations.head())
+
+    data_files = ['visualisations/listings.csv', 'visualisations/reviews.csv']
+    dates = airbnb.lookup('dates', for_='Francisco')
+    print(dates)
+    middle_date = dates[len(dates) // 2]
+    sf_data = airbnb.get_data(location_name='Francisco', date=middle_date, data_files=data_files)
+    print(sf_data)
