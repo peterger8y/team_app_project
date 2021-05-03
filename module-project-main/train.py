ee1abe61a1f4cd93eee2bd0dded5125abbf04406
peterger8y <73508520+peterger8y@users.noreply.github.com>
2021-05-03 01:26:43 -0500

    Update train.py

--- module-project-main/train.py
+++ module-project-main/train.py
@@ -1,17 +1,33 @@
-from inside_airbnb import InsideAirBnB
+from .inside_airbnb import InsideAirBnB
+import pandas as pd
+import geopandas as gpd
+
+locations = InsideAirBnB()
+list_locations = list(locations.load_locations().get('name'))
 
 
-class Train:
-    def __init__(self):
-        self.data = self.get_data()
-
-    def get_data(self):
-        airbnb = InsideAirBnB()
-        data_files = ['data/listings.csv.gz']
-        data = airbnb.get_data(location_name='New York City', date='2021-02-04', data_files=data_files)
-        return data
+def neighbourhood_function(alpha):
+    locations = InsideAirBnB()
+    delta = locations.get_data(location_name=alpha, date=None, data_files=['data/listings.csv.gz'])
+    epsilon = delta['listings-dat']['neighbourhood_cleansed']
+    zeta = pd.DataFrame(epsilon.value_counts()).index
+    return zeta
 
 
-if __name__ == "__main__":
-    train = Train()
-    print(train.data['listings-dat'].head())
+# if __name__ == "__main__":
+# for x in beta:
+
+def get_at_it(location, geo=False):
+    locations = InsideAirBnB()
+    if geo:
+        scepter = locations.get_data(location_name=location, date=None, data_files=['visualisations/listings.csv'])
+        geo_df = scepter['listings-vis']
+        eighty = locations.get_data(location_name=location, date=None, data_files=['visualisations/neighbourhoods.geojson'])
+        #alpha = eighty['neighborhoods-geo']
+        geo_df = geo_df.groupby(by=["neighbourhood"]).mean()
+
+        return eighty['neighbourhoods-geo'], geo_df
+    omega = locations.get_data(location_name=location, date=None, data_files=['data/listings.csv.gz'])
+    kappa = omega['listings-dat']
+
+    return kappa
